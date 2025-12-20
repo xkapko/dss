c7dba4a10875db0572b72eaf8a8fb9dcdeea0ded
xkapko <536648@mail.muni.cz>
2025-12-20 16:19:53 +0100

    drawing using raylib

--- src/bin/bsp.rs
+++ src/bin/bsp.rs
@@ -1,141 +1,74 @@
 extern crate dss;
 
 use dss::data_structures::bsptree::{BSPTree, MoveDirection, Rectangle};
+use raylib::color::Color;
+use raylib::prelude::{RaylibDraw, RaylibDrawHandle, Rectangle as RRect};
 
-use std::cell::RefCell;
-use std::rc::Rc;
-
-use std::os::raw::c_double;
-use tcl::*;
-use tk::{cmd::*, *};
-
-fn convert(rect: Rectangle) -> ((f64, f64), (f64, f64)) {
-    let (x, y) = (rect.x as f32, rect.y as f32);
-    let (nx, ny) = (x + rect.w as f32, y + rect.h as f32);
-
-    ((x as f64, y as f64), (nx as f64, ny as f64))
+fn convert(rect: Rectangle) -> RRect {
+    RRect::new(rect.x as f32, rect.y as f32, rect.w as f32, rect.h as f32)
 }
 
-fn draw_tree(tree: Rc<RefCell<BSPTree>>, c: TkCanvas<impl Fn() + Copy + 'static>) -> TkResult<()> {
-    let nodes = tree.borrow_mut().walk();
-    c.create_rectangle(0., 0., 640., 480., -fill("white"))?;
-    for node in nodes {
+fn draw_tree(tree: &BSPTree, mut c: RaylibDrawHandle) -> Result<(), Box<dyn std::error::Error>> {
+    for node in tree.walk() {
         let n = node.borrow();
         let rect = n.get_rect();
         if n.get_data().is_some() {
-            let (fst, snd) = convert(rect);
+            let rect = convert(rect);
             if n.is_focused() {
-                c.create_rectangle(fst.0, fst.1, snd.0, snd.1, -fill("green"))?;
+                c.draw_rectangle_rec(rect, Color::GREEN);
+                c.draw_rectangle_lines_ex(rect, 3., Color::BLACK);
             } else {
-                c.create_rectangle(fst.0, fst.1, snd.0, snd.1, -fill("red"))?;
+                c.draw_rectangle_rec(rect, Color::RED);
+                c.draw_rectangle_lines_ex(rect, 3., Color::BLACK);
             }
         } else {
-            let (fst, snd) = convert(rect);
-            c.create_rectangle(fst.0, fst.1, snd.0, snd.1, -fill("blue"))?;
+            let rect = convert(rect);
+            c.draw_rectangle_rec(rect, Color::BLUE);
+            c.draw_rectangle_lines_ex(rect, 3.0, Color::BLACK);
         }
     }
     println!("===");
-    tree.borrow().print(1);
+    tree.print(1);
     println!("===");
     Ok(())
 }
 
-fn main() -> TkResult<()> {
-    let tree = Rc::new(RefCell::new(BSPTree::new(Rectangle::new(0, 0, 640, 480))));
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (w, h): (i32, i32) = (640, 480);
+    let mut tree = BSPTree::new(Rectangle::new(0, 0, w as u32, h as u32));
 
-    let tk = make_tk!()?;
-    let root = tk.root();
+    let (mut rl, thread) = raylib::init().size(w, h).title("BSPTree demo").build();
+    let mut count = 0;
 
-    let c = root.add_canvas("canvas" - width(640) - height(480) - background("white"))?;
-    c.pack(())?;
-
-    let t = tree.clone();
-
-    let count = Rc::new(RefCell::new(0));
-
-    root.bind(
-        event::key_press(TkKey::n),
-        tclosure!(tk, args: "%x %y", move |_x: c_double, _y: c_double| -> TkResult<()> {
-            let co = *count.borrow();
-            t.borrow_mut().insert(co + 1);
-            draw_tree(t.clone(), c)?;
-            count.replace(co + 1);
-            Ok(())
-        }),
-    )?;
-    let t = tree.clone();
-    root.bind(
-        event::key_press(TkKey::d),
-        tclosure!(tk, args: "%x %y", move |_x: c_double, _y: c_double| -> TkResult<()> {
-            t.borrow_mut().delete_focused();
-            draw_tree(t.clone(), c)?;
-            Ok(())
-        }),
-    )?;
-
-    let t = tree.clone();
-    root.bind(
-        event::key_press(TkKey::s),
-        tclosure!(tk, args: "%x %y", move |_x: c_double, _y: c_double| -> TkResult<()> {
-            t.borrow_mut().toggle_split();
-            draw_tree(t.clone(), c)?;
-            Ok(())
-        }),
-    )?;
-
-    let t = tree.clone();
-    root.bind(
-        event::button_press_1(),
-        tclosure!(tk, args: "%x %y", move |x: c_double, y: c_double| -> TkResult<()> {
-            t.borrow_mut().focus_coords(x as i32, y as i32);
-            draw_tree(t.clone(), c)?;
-            Ok(())
-        }),
-    )?;
-    let t = tree.clone();
-    root.bind(
-        event::key_press(TkKey::h),
-        tclosure!(tk, args: "%x %y", move |_x: c_double, _y: c_double| -> TkResult<()> {
-            t.borrow_mut().move_focus(MoveDirection::Left);
-            draw_tree(t.clone(), c)?;
-            Ok(())
-        }),
-    )?;
-    let t = tree.clone();
-    root.bind(
-        event::key_press(TkKey::l),
-        tclosure!(tk, args: "%x %y", move |_x: c_double, _y: c_double| -> TkResult<()> {
-            t.borrow_mut().move_focus(MoveDirection::Right);
-            draw_tree(t.clone(), c)?;
-            Ok(())
-        }),
-    )?;
-    let t = tree.clone();
-    root.bind(
-        event::key_press(TkKey::k),
-        tclosure!(tk, args: "%x %y", move |_x: c_double, _y: c_double| -> TkResult<()> {
-            t.borrow_mut().move_focus(MoveDirection::Up);
-            draw_tree(t.clone(), c)?;
-            Ok(())
-        }),
-    )?;
-    let t = tree.clone();
-    root.bind(
-        event::key_press(TkKey::j),
-        tclosure!(tk, args: "%x %y", move |_x: c_double, _y: c_double| -> TkResult<()> {
-            t.borrow_mut().move_focus(MoveDirection::Down);
-            draw_tree(t.clone(), c)?;
-            Ok(())
-        }),
-    )?;
-
-    tree.borrow_mut().delete_focused();
-
-    draw_tree(tree.clone(), c)?;
-
-    main_loop();
-
-    tree.borrow().print(1);
+    while !rl.window_should_close() {
+        if let Some(key) = rl.get_key_pressed() {
+            match key {
+                // left
+                raylib::ffi::KeyboardKey::KEY_H => tree.move_focus(MoveDirection::Left),
+                // down
+                raylib::ffi::KeyboardKey::KEY_J => tree.move_focus(MoveDirection::Down),
+                // up
+                raylib::ffi::KeyboardKey::KEY_K => tree.move_focus(MoveDirection::Up),
+                // right
+                raylib::ffi::KeyboardKey::KEY_L => tree.move_focus(MoveDirection::Right),
+                // delete
+                raylib::ffi::KeyboardKey::KEY_D => tree.delete_focused(),
+                // toggle layout
+                raylib::ffi::KeyboardKey::KEY_S => tree.toggle_split(),
+                // new client
+                raylib::ffi::KeyboardKey::KEY_N => {
+                    count += 1;
+                    tree.insert(count)
+                }
+                _ => {}
+            }
+        }
+        let pos = rl.get_mouse_position();
+        tree.focus_coords(pos.x as i32, pos.y as i32);
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        draw_tree(&tree, d)?;
+    }
 
     Ok(())
 }
